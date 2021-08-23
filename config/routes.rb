b6e234a2f7e7a1962b58123a51911d45738646ee
Thibault Lenclos <thiblenclos@gmail.com>
2021-08-23 18:16:01 +0200

    controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :wishes do
+    resources :likes, only: [:create]
+  end
+
+  resources :matches, only: [:show] do
+    resources :locations, only: [:show, :index]
+  end
 end
