ea7fd05c3d5d1a1d1b9b54494d45265df063a191
O neal Maoukola <maoukola.oneal@gmail.com>
2021-08-24 17:22:53 +0200

    finishing wishes index

--- app/policies/wish_policy.rb
+++ app/policies/wish_policy.rb
@@ -1,7 +1,7 @@
 class WishPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(sport: user.wishes.last.sport).where.not(user: user)
     end
   end
 
