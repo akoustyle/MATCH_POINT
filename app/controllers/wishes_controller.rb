7467eb63fc1a63cde443350ab5e4431567794ced
O neal Maoukola <maoukola.oneal@gmail.com>
2021-08-24 13:14:01 +0200

    view et controller de wish + maj de views devise registration et sessions

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,7 +1,6 @@
 class WishesController < ApplicationController
   def index
-    @wishes = Wish.all
-    # @wishes = policy_scope(Wish).order(created_at: :desc)
+    @wishes = policy_scope(Wish).order(created_at: :desc)
 
   #   if params.dig(:search, :query).present?
   #     @wishes = @wishes.search_by_sport(params.dig(:search, :query))
