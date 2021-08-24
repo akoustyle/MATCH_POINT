0af98589c611d16ad387776825d398691d0d8c08
Thibault Lenclos <thiblenclos@gmail.com>
2021-08-24 11:51:09 +0200

    sport seed created

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,5 +1,7 @@
 class Sport < ApplicationRecord
-  has_many :wishes, :locations, :preferences
+  has_many :wishes
+  has_many :locations
+  has_many :preferences
 
   validates :name, presence: true, uniqueness: true
 end
