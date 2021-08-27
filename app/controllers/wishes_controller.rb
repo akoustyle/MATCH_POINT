a9ca40e324e5361a5b71b662203c5b6c7ae8c6c0
O neal Maoukola <maoukola.oneal@gmail.com>
2021-08-27 15:16:05 +0200

    updating wish location before merge

--- app/controllers/wishes_controller.rb
+++ app/controllers/wishes_controller.rb
@@ -1,10 +1,12 @@
 class WishesController < ApplicationController
   def index
     @wishes = policy_scope(Wish).order(created_at: :desc)
-
+    @user_wish = current_user.wishes.where(date: Date.today).last
   #   if params.dig(:search, :query).present?
   #     @wishes = @wishes.search_by_sport(params.dig(:search, :query))
   end
+
+
 
   def show
 
@@ -20,6 +22,7 @@ class WishesController < ApplicationController
     @sport = Sport.find_by(name: params[:wish][:sport])
     @wish.sport = @sport
     @wish.date = Date.today
+    @wish.location = Location.last.address
     authorize @wish
     if @wish.save
       redirect_to wishes_path
