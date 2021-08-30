09c414639b2070abc151fda2f5d66a201bb978e9
O neal Maoukola <maoukola.oneal@gmail.com>
2021-08-30 14:15:06 +0200

    chatroom ready missing css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_154210) do
+ActiveRecord::Schema.define(version: 2021_08_30_084050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2021_08_25_154210) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "likes", force: :cascade do |t|
@@ -62,6 +68,16 @@ ActiveRecord::Schema.define(version: 2021_08_25_154210) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["location_id"], name: "index_matches_on_location_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "preferences", force: :cascade do |t|
@@ -114,6 +130,8 @@ ActiveRecord::Schema.define(version: 2021_08_25_154210) do
   add_foreign_key "likes", "wishes"
   add_foreign_key "locations", "sports"
   add_foreign_key "matches", "locations"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "preferences", "sports"
   add_foreign_key "preferences", "users"
   add_foreign_key "wishes", "matches"
