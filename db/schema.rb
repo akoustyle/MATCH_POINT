6bda4cb25320142164672cfa7d304ba08c479aee
Stephane Mouillat <stephane.mouillat@live.fr>
2021-08-30 16:49:32 +0200

    Migration status pour les matches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_25_154210) do
+ActiveRecord::Schema.define(version: 2021_08_30_144631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_08_25_154210) do
     t.bigint "location_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["location_id"], name: "index_matches_on_location_id"
   end
 
