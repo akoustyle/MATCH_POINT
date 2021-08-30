ce60e63ed8cc6af0972d46e4dc2c01a86b13b6f1
Stephane Mouillat <stephane.mouillat@live.fr>
2021-08-30 14:46:27 +0200

    Changements qui servent à rien sur le destroy des matches

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :likes, only: [:create]
   end
 
-  resources :matches, only: [:show, :index, :create] do
+  resources :matches, only: [:show, :index, :create, :destroy] do
     resources :locations, only: [:show, :index]
   end
 end
