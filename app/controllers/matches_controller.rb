c86c8265ec6f874768ec844c59e5baabb792de2c
O neal Maoukola <maoukola.oneal@gmail.com>
2021-09-03 12:36:27 +0200

    location fixed

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,18 +3,25 @@ class MatchesController < ApplicationController
     @match = Match.find(params[:id])
     authorize @match
     locations = Location.where(sport_id: @match.wishes.first.sport_id)
+    @location = locations[0];
+    @marker = [{
+      lat: @location.latitude,
+      lng: @location.longitude,
+      info_window: render_to_string(partial: "info_window", locals: { location: @location }),
+      image_url: helpers.asset_url('blue-marker.jpg')
+    }]
     # @match.wishes.each do |wish|
-    locations.each do |location|
-      if @match.wishes.last.user != current_user
-        @location = location
-        @marker =[{
-          lat: location.latitude,
-          lng: location.longitude,
-          info_window: render_to_string(partial: "info_window", locals: { location: location }),
-          image_url: helpers.asset_url('blue-marker.jpg')
-        }]
-      end
-    end
+    # locations.each do |location|
+    #   if @match.wishes.last.user != current_user
+    #     @location = location
+    #     @marker =[{
+    #       lat: location.latitude,
+    #       lng: location.longitude,
+    #       info_window: render_to_string(partial: "info_window", locals: { location: location }),
+    #       image_url: helpers.asset_url('blue-marker.jpg')
+    #     }]
+    #   end
+    # end
   end
 
   def index
